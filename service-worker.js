42bf764c32941ded9b3e38ddfc1cb35c267ef943
Jarod-Hodr <jprussell1997@gmail.com>
2025-11-15 09:25:47 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,27 @@
 self.addEventListener('install', event => {
-event.waitUntil(
-caches.open('snowfall-cache-v1').then(cache => {
-return cache.addAll([
-'./',
-'./index.html',
-'./manifest.json'
-]);
-})
-);
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open('snowfall-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icons/icon-192.png',
+        '/icons/icon-512.png'
+      ]);
+    })
+  );
 });
 
+self.addEventListener('activate', event => {
+  clients.claim();
+});
 
 self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request).then(response => {
-return response || fetch(event.request);
-})
-);
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
